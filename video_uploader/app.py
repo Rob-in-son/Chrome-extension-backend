9537e28d6b3c2f7dfe7124ac22aefa6219364464
Rob-in-son <estherchidinma202@gmail.com>
2023-09-28 20:15:17 +0100

    Files are now working together lol

--- video_uploader/app.py
+++ video_uploader/app.py
@@ -1,12 +1,16 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+import config
 import os
 
 #Initialize app
 app = Flask(__name__)
 #Initialize CORS
 CORS(app)
+
+#Load db config from config.py file
+app.config.from_object(config.Config)
 
 #Initialize sqlalchemy
 db = SQLAlchemy(app)
--- video_uploader/config.py
+++ video_uploader/config.py
@@ -1,4 +1,5 @@
 #Configure database
+import os
 
 #The absolute path of the current folder 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -6,5 +7,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
 
     #Configure sqlite database
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
