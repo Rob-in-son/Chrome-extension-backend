4888ae0a8f4eac4594af6e7bf305e976b8e0c228
Rob-in-son <estherchidinma202@gmail.com>
2023-09-28 18:10:07 +0100

    Blueprint

--- video_uploader/video.py
+++ video_uploader/video.py
@@ -1,7 +1,12 @@
-from video_uploader import db
+from chrome_extension import db
 # video class/model
 class Video(db.Model):
+    __tablename__ = 'videos'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     video_url = db.Column(db.string(200), unique=True, nullable=False)
+
+    def __init__(self, video_url):
+        self.video_url = video_url
+
 
 
